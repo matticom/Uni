7b424c172ad684dedb819a35e295a84aacea4184
Matthias Kugler <matti.com@gmail.com>
2017-04-20 17:50:13 +0200

    mergesort

--- Uni/src/aud/a02/IntegerComparator.java
+++ Uni/src/aud/a02/IntegerComparator.java
@@ -8,5 +8,4 @@ public class IntegerComparator implements Comparator<Integer>{
 	public int compare(Integer o1, Integer o2) {
 		return o1.compareTo(o2);
 	}
-
 }
--- Uni/src/aud/a02/MergeSortOptimizeObject.java
+++ Uni/src/aud/a02/MergeSortOptimizeObject.java
@@ -1,107 +1,130 @@
 package aud.a02;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 import aud.a01.SelectSort;
 import java.time.Instant;
 
 public class MergeSortOptimizeObject<T> {
-	private List<T> auxArray;
-	private List<T> array;
-	private Comparator<? super T> comparator;
+	private Object[] auxArray;
+	private T[] array;
+	private Comparator<T> comparator;
 	
 		
-	private MergeSortOptimizeObject(List<T> array, Comparator<? super T> comparator) {
+	private MergeSortOptimizeObject(T[] array, Comparator<T> comparator) {
+		// Initialisierung der Objektproperties
 		this.array = array;
 		this.comparator = comparator;
-		auxArray = new ArrayList<T>(array.size());
-		int r = array.size()-1;
+		int r = array.length-1;
+		auxArray = new Object[r+1];
+		
+		// Algorithmusstart
 		mergeSort(0, r);
 	}
-
-	public static <T> void sort(List<T> list, Comparator<? super T> comparator){
-		MergeSortOptimizeObject<T> mergeSortOptimizeObject = new MergeSortOptimizeObject<T>(list, comparator);
-		
+	
+	// Statische Aufrufmethode zum Sortieren mit typisierten Array und typisierten Komparator 
+	public static <T> void sort(T[] array, Comparator<T> comparator){
+		new MergeSortOptimizeObject<T>(array, comparator);
 	}
-			
+		
+	// rekursive Methode zum Zerlegen und zusammen mischen des Arrays 
 	private void mergeSort(int l, int r) {
 		int m;
+		// Abbruchbedingung: solange rechter und linker Index nicht gleich
 		if (l < r) {
-			m = (l+r) / 2;
-			mergeSort(l, m);
-			mergeSort(m+1, r);
-			merge(l, m, r);
+			m = (l+r) / 2; 		// Mitte des Arrays
+			mergeSort(l, m);	// rekursiver Aufruf 1. Hälfte des Arrays
+			mergeSort(m+1, r);	// rekursiver Aufruf 2. Hälfte des Arrays
+			merge(l, m, r);		// beide Hälften zusammen mischen
 		}
 	}
 	
 	private void merge(int l, int m, int r) {
+		// 	beide Arrayhälften in Hilfsarray kopieren
+		//  Optimierte Variante: rechte Hälfte wird in umgedrehter Reihenfolge
+		// 	ins Hilfsarray kopiert
 		copyToAuxArray(l, m, r);
+		//	Zeiger auf das zu sortierende Array auf linke Index setzen
 		int arrayIdx = l;
+		//  Optimierte Variante: linker und rechter Indexzeiger bewegen sich
+		//	aufeinander zu bis l und r den gleichen Index haben 
 		while( l != r ) {
-			if ((comparator.compare(auxArray.get(l), auxArray.get(r))) >= 0){//auxArray[l] >= auxArray[r]) {
-				array.set(arrayIdx++, auxArray.get(r--));
+			//	mit Komparator vergleichen, ob rechter oder linker Wert kleiner ist
+			//	downcast Object zu T, da compare Parametertyp T verlangt
+			if ((comparator.compare((T)auxArray[l], (T)auxArray[r])) >= 0) {
+				//	Wenn rechter Wert kleiner ist: rechte Werte ins Hauptarray
+				//	kopieren, arrayIndex 1 erhöhen und rechten auxArrayIndex 
+				//	um 1 verringern
+				array[arrayIdx++] = (T)auxArray[r--];
 			} else {
-				array.set(arrayIdx++, auxArray.get(l++));
+				//	Wenn linker Wert kleiner ist: linke Werte ins Hauptarray
+				//	kopieren, arrayIndex 1 erhöhen und linken auxArrayIndex 
+				//	um 1 erhöhen
+				array[arrayIdx++] = (T)auxArray[l++];
 			}
 		}
-		array.set(arrayIdx, auxArray.get(r));
+		//	am Ende den Wert beim r=l Index ins Hauptarray kopieren
+		array[arrayIdx] = (T)auxArray[r];
 	}
 	
 	private void copyToAuxArray(int l, int m, int r) {
-		System.out.println(array.get(0));
-//		auxArray.set(0, );
+		//	linke Hälfte vom Hauptarray 1:1 ins Hilfsarray kopieren
 		for(int i = l; i <= m; i++) {
-			auxArray.set(i, array.get(i));
+			auxArray[i] = array[i];
 		}
 		int j = m+1;
+		//	rechte Hälfte vom Hauptarray in umgedrehter Reihenfolge
+		//	ins Hilfsarray kopieren
 		for(int i = r; i > m; i--) {
-			auxArray.set(i, array.get(j++));
+			auxArray[i] = array[j++];
 		}
 	}
 	
 	public static void main(String[] args) {
+		
+		final int ELEMENTS = 100000;
+		
 		Instant start, end;
 		Long startLong, endLong;
 		Random random = new Random();
-		int[] testArray = new int[100];
-		for(int i=0; i<100; i++) {
-			testArray[i] = random.nextInt(100)-50;
+		int[] testArraySelectSort = new int[ELEMENTS];
+		int[] testArrayMergeSort = new int[ELEMENTS];
+		Integer[] objectTestArrayMergeSort = new Integer[ELEMENTS];
+		
+		for(int i=0; i<ELEMENTS; i++) {
+			int randomNumber = random.nextInt(ELEMENTS) - ELEMENTS/2;
+			testArraySelectSort[i] = randomNumber;
+			testArrayMergeSort[i] = randomNumber;
+			objectTestArrayMergeSort[i] = new Integer(randomNumber);
 		}
-		
-		List<Integer> list = Arrays.stream(testArray).boxed().collect(Collectors.toList());
-				
-		
-		
+						
 		MergeSortOptimize mergeSortOptimize = new MergeSortOptimize();
 		SelectSort selectSort = new SelectSort();
 		
-//		start = Instant.now();
-//		startLong = start.toEpochMilli();
-//		System.out.println(Arrays.toString(selectSort.sortArray(testArray)));
-//		end = Instant.now();
-//		endLong = end.toEpochMilli();
-//		System.out.println("Verbrauchte Zeit für Sortieren und darstellen mit SelectSort [ms]: " + (endLong-startLong));
-//		System.out.println();
-//		
-//		start = Instant.now();
-//		startLong = start.toEpochMilli();
-//		System.out.println(Arrays.toString(mergeSortOptimize.sortArrayWithMergeSort(testArray)));
-//		end = Instant.now();
-//		endLong = end.toEpochMilli();
-//		System.out.println("Verbrauchte Zeit für Sortieren und darstellen mit optimierten MergeSort [ms]: " + (endLong-startLong));
-		
 		start = Instant.now();
 		startLong = start.toEpochMilli();
-		MergeSortOptimizeObject.sort(list, new IntegerComparator());
-		System.out.println(list.stream().map(Object::toString).collect(Collectors.joining(", ")));
+		MergeSortOptimizeObject.sort(objectTestArrayMergeSort, new IntegerComparator());
 		end = Instant.now();
 		endLong = end.toEpochMilli();
-		System.out.println("Verbrauchte Zeit für Sortieren und darstellen mit optimierten MergeSort mit Objekten [ms]: " + (endLong-startLong));
+		System.out.println(Arrays.toString(objectTestArrayMergeSort));
+		System.out.println("Verbrauchte Zeit für Sortieren mit optimierten MergeSort mit Objekten [ms]: " + (endLong-startLong) + "\n");
+			
+		start = Instant.now();
+		startLong = start.toEpochMilli();
+		mergeSortOptimize.sortArrayWithMergeSort(testArrayMergeSort);
+		end = Instant.now();
+		endLong = end.toEpochMilli();
+		System.out.println(Arrays.toString(testArrayMergeSort));
+		System.out.println("Verbrauchte Zeit für Sortieren mit optimierten MergeSort [ms]: " + (endLong-startLong) + "\n");
+				
+		start = Instant.now();
+		startLong = start.toEpochMilli();
+		selectSort.sortArray(testArraySelectSort);
+		end = Instant.now();
+		endLong = end.toEpochMilli();
+		System.out.println(Arrays.toString(testArraySelectSort));
+		System.out.println("Verbrauchte Zeit für Sortieren mit SelectSort [ms]: " + (endLong-startLong) + "\n");
 	}
 }
