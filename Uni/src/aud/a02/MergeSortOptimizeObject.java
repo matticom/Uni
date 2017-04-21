726fd894fc477deefda6dd86842be1017422c265
Matthias Kugler <matti.com@gmail.com>
2017-04-21 18:02:45 +0200

    Test fertig

--- Uni/src/aud/a02/MergeSortOptimizeObject.java
+++ Uni/src/aud/a02/MergeSortOptimizeObject.java
@@ -1,11 +1,6 @@
 package aud.a02;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Random;
-
-import aud.a01.SelectSort;
-import java.time.Instant;
 
 public class MergeSortOptimizeObject<T> {
 	private Object[] auxArray;
@@ -14,117 +9,48 @@ public class MergeSortOptimizeObject<T> {
 	
 		
 	private MergeSortOptimizeObject(T[] array, Comparator<T> comparator) {
-		// Initialisierung der Objektproperties
 		this.array = array;
 		this.comparator = comparator;
 		int r = array.length-1;
 		auxArray = new Object[r+1];
 		
-		// Algorithmusstart
 		mergeSort(0, r);
 	}
 	
-	// Statische Aufrufmethode zum Sortieren mit typisierten Array und typisierten Komparator 
 	public static <T> void sort(T[] array, Comparator<T> comparator){
 		new MergeSortOptimizeObject<T>(array, comparator);
 	}
 		
-	// rekursive Methode zum Zerlegen und zusammen mischen des Arrays 
 	private void mergeSort(int l, int r) {
 		int m;
-		// Abbruchbedingung: solange rechter und linker Index nicht gleich
 		if (l < r) {
-			m = (l+r) / 2; 		// Mitte des Arrays
-			mergeSort(l, m);	// rekursiver Aufruf 1. Hälfte des Arrays
-			mergeSort(m+1, r);	// rekursiver Aufruf 2. Hälfte des Arrays
-			merge(l, m, r);		// beide Hälften zusammen mischen
+			m = (l+r) / 2;
+			mergeSort(l, m);
+			mergeSort(m+1, r);
+			merge(l, m, r);
 		}
 	}
 	
 	private void merge(int l, int m, int r) {
-		// 	beide Arrayhälften in Hilfsarray kopieren
-		//  Optimierte Variante: rechte Hälfte wird in umgedrehter Reihenfolge
-		// 	ins Hilfsarray kopiert
 		copyToAuxArray(l, m, r);
-		//	Zeiger auf das zu sortierende Array auf linke Index setzen
 		int arrayIdx = l;
-		//  Optimierte Variante: linker und rechter Indexzeiger bewegen sich
-		//	aufeinander zu bis l und r den gleichen Index haben 
 		while( l != r ) {
-			//	mit Komparator vergleichen, ob rechter oder linker Wert kleiner ist
-			//	downcast Object zu T, da compare Parametertyp T verlangt
 			if ((comparator.compare((T)auxArray[l], (T)auxArray[r])) >= 0) {
-				//	Wenn rechter Wert kleiner ist: rechte Werte ins Hauptarray
-				//	kopieren, arrayIndex 1 erhöhen und rechten auxArrayIndex 
-				//	um 1 verringern
 				array[arrayIdx++] = (T)auxArray[r--];
 			} else {
-				//	Wenn linker Wert kleiner ist: linke Werte ins Hauptarray
-				//	kopieren, arrayIndex 1 erhöhen und linken auxArrayIndex 
-				//	um 1 erhöhen
 				array[arrayIdx++] = (T)auxArray[l++];
 			}
 		}
-		//	am Ende den Wert beim r=l Index ins Hauptarray kopieren
 		array[arrayIdx] = (T)auxArray[r];
 	}
 	
 	private void copyToAuxArray(int l, int m, int r) {
-		//	linke Hälfte vom Hauptarray 1:1 ins Hilfsarray kopieren
 		for(int i = l; i <= m; i++) {
 			auxArray[i] = array[i];
 		}
 		int j = m+1;
-		//	rechte Hälfte vom Hauptarray in umgedrehter Reihenfolge
-		//	ins Hilfsarray kopieren
 		for(int i = r; i > m; i--) {
 			auxArray[i] = array[j++];
 		}
-	}
-	
-	public static void main(String[] args) {
-		
-		final int ELEMENTS = 100000;
-		
-		Instant start, end;
-		Long startLong, endLong;
-		Random random = new Random();
-		int[] testArraySelectSort = new int[ELEMENTS];
-		int[] testArrayMergeSort = new int[ELEMENTS];
-		Integer[] objectTestArrayMergeSort = new Integer[ELEMENTS];
-		
-		for(int i=0; i<ELEMENTS; i++) {
-			int randomNumber = random.nextInt(ELEMENTS) - ELEMENTS/2;
-			testArraySelectSort[i] = randomNumber;
-			testArrayMergeSort[i] = randomNumber;
-			objectTestArrayMergeSort[i] = new Integer(randomNumber);
-		}
-						
-		MergeSortOptimize mergeSortOptimize = new MergeSortOptimize();
-		SelectSort selectSort = new SelectSort();
-		
-		start = Instant.now();
-		startLong = start.toEpochMilli();
-		MergeSortOptimizeObject.sort(objectTestArrayMergeSort, new IntegerComparator());
-		end = Instant.now();
-		endLong = end.toEpochMilli();
-		System.out.println(Arrays.toString(objectTestArrayMergeSort));
-		System.out.println("Verbrauchte Zeit für Sortieren mit optimierten MergeSort mit Objekten [ms]: " + (endLong-startLong) + "\n");
-			
-		start = Instant.now();
-		startLong = start.toEpochMilli();
-		mergeSortOptimize.sortArrayWithMergeSort(testArrayMergeSort);
-		end = Instant.now();
-		endLong = end.toEpochMilli();
-		System.out.println(Arrays.toString(testArrayMergeSort));
-		System.out.println("Verbrauchte Zeit für Sortieren mit optimierten MergeSort [ms]: " + (endLong-startLong) + "\n");
-				
-		start = Instant.now();
-		startLong = start.toEpochMilli();
-		selectSort.sortArray(testArraySelectSort);
-		end = Instant.now();
-		endLong = end.toEpochMilli();
-		System.out.println(Arrays.toString(testArraySelectSort));
-		System.out.println("Verbrauchte Zeit für Sortieren mit SelectSort [ms]: " + (endLong-startLong) + "\n");
 	}
 }
--- Uni/src/aud/a02/MergeSortTest.java
+++ Uni/src/aud/a02/MergeSortTest.java
@@ -1,43 +1,104 @@
 package aud.a02;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+import static aud.a02.testArrays.FileIntArray.FileToIntArray;
+
+@RunWith(Parameterized.class)
 public class MergeSortTest {
 
-	private Integer[] array;
 	private Comparator<Integer> comparator;
 	private Instant start, end;
-	private Long startLong, endLong;
-	
+	private String testArrayFile;
+			
+	public MergeSortTest(String testArrayFile) {
+		this.testArrayFile = testArrayFile;
+	}
+
 	@Before
 	public void setUp() {
-		array = new Integer[]{new Integer(3),new Integer(6),new Integer(1),new Integer(2),new Integer(4),new Integer(3),new Integer(6)};
 		comparator = new IntegerComparator();
+	}
+	
+	@Parameters
+	public static List<Object> data() {
+	    return generateTestArrayList();
 	}
 	
 	@Test
 	public void testMergeSort() {
+		Integer[] array = convertIntToInteger(FileToIntArray(testArrayFile));
+		
 		start = Instant.now();
 		MergeSortOptimizeObject.sort(array, comparator);
 		end = Instant.now();
 		
-		startLong = start.toEpochMilli();
-		endLong = end.toEpochMilli();
-		System.out.println("Verbrauchte Zeit für Sortieren mit optimierten MergeSort mit Objekten [ms]: " + (endLong-startLong) + "\n");
-		Assert.assertEquals(true, isSorted());
+//		System.out.println(Arrays.toString(array));
+		System.out.println("Aktuelles Testarray: " + testArrayFile);
+		System.out.println("Verbrauchte Zeit für Sortieren mit optimiertem MergeSort [ms]: " + calNeededTime() + "\n");
+		Assert.assertEquals(true, isSorted(array));
 	}
 	
-	private boolean isSorted() {
+	private boolean isSorted(Integer[] array) {
 		for(int i = 1; i < array.length; i++) {
 			if (comparator.compare(array[i-1], array[i]) > 0) {
 				return false;
 			}
 		}
 		return true;
+	}
+	
+	private Integer[] convertIntToInteger(int[] array) {
+		Integer[] objArray = new Integer[array.length];
+		for (int i = 0; i < array.length; i++) {
+			objArray[i] = new Integer(array[i]); 
+		}
+		return objArray;
+	}
+	
+	private long calNeededTime() {
+		return end.toEpochMilli()-start.toEpochMilli();
+	}
+	
+	private static List<Object> generateTestArrayList() {
+		return Arrays.asList(new Object[] {
+				"./src/aud/a02/testArrays/Rand10_2",
+				"./src/aud/a02/testArrays/Rand10_2",
+				"./src/aud/a02/testArrays/Rand100_1",
+				"./src/aud/a02/testArrays/Rand100_2",
+				"./src/aud/a02/testArrays/Rand1000_1",
+				"./src/aud/a02/testArrays/Rand1000_2",
+				"./src/aud/a02/testArrays/Rand10000_1",
+				"./src/aud/a02/testArrays/Rand10000_2",
+				"./src/aud/a02/testArrays/Rand100000_1",
+				"./src/aud/a02/testArrays/Rand100000_2",
+				"./src/aud/a02/testArrays/Rand20_1",
+				"./src/aud/a02/testArrays/Rand20_2",
+				"./src/aud/a02/testArrays/Rand200_1",
+				"./src/aud/a02/testArrays/Rand200_2",
+				"./src/aud/a02/testArrays/Rand50_1",
+				"./src/aud/a02/testArrays/Rand50_2",
+				"./src/aud/a02/testArrays/Rand500_1",
+				"./src/aud/a02/testArrays/Rand500_2",
+				"./src/aud/a02/testArrays/Sort10_1",
+				"./src/aud/a02/testArrays/Sort100_1",
+				"./src/aud/a02/testArrays/Sort1000_1",
+				"./src/aud/a02/testArrays/Sort10000_1",
+				"./src/aud/a02/testArrays/Sort100000_1",
+				"./src/aud/a02/testArrays/Sort20_1",
+				"./src/aud/a02/testArrays/Sort200_1",
+				"./src/aud/a02/testArrays/Sort50_1",
+				"./src/aud/a02/testArrays/Sort500_1",
+				});
 	}
 }
