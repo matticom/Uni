a3bdc60b329774cca290508cd69e7aa90016211d
Matthias Kugler <matti.com@gmail.com>
2017-05-10 16:50:25 +0200

    queue endversion

--- Uni/src/aud/a03/Queue.java
+++ Uni/src/aud/a03/Queue.java
@@ -7,49 +7,45 @@ public class Queue <T> {
 	protected int firstFreeIndex;
 	protected int firstUsedIndex;
 	protected Object[] array;
-	protected int size;
-	protected final int LAST_ARRAY_INDEX;
+	protected int queueSize;
 	protected boolean queueIsFull;
 	protected boolean queueIsEmpty;
 	
-	public Queue(int size) {
-		this.size = size;
-		array = new Object[size];
+	public Queue(int queueSize) {
+		this.queueSize = queueSize;
+		array = new Object[queueSize];
 		firstFreeIndex = 0;
 		firstUsedIndex = 0;
-		LAST_ARRAY_INDEX = size-1;
 		queueIsFull = false;
 		queueIsEmpty = true;
 	}
 	
-	public Queue() {
-		this(10);
-	}
-	
-	// Enqueue-Methode: Laufzeit in O-Notation beträgt o(1), da alle Anweisungen der Methode o(1) haben
+	// Enqueue-Methode: Laufzeit in O-Notation beträgt o(1), 
+	// da alle Anweisungen der Methode o(1) haben
 	public void enqueue(T obj) throws QueueOverflow {
 		if (queueIsFull) {
 			throw new QueueOverflow("Queue ist voll");
 		}
-		// Queue voll, wenn beide Zeiger nach enqueue auf gleichen Index zeigen 
-		// (Modulo-Funktion um Array als Ringspeicher zu betrachten)
-		queueIsFull = (firstUsedIndex == (firstFreeIndex+1) % size);
+		// Queue ist voll, wenn beide Zeiger nach enqueue auf gleichen Index zeigen 
+		// (Modulo-Funktion um Array als Ringspeicher zu verwenden)
+		queueIsFull = (firstUsedIndex == (firstFreeIndex+1) % queueSize);
 		array[firstFreeIndex] = (T) obj;
-		firstFreeIndex = ++firstFreeIndex % size;
+		firstFreeIndex = ++firstFreeIndex % queueSize;
 		queueIsEmpty = false;
 	}
 	
-	// Dequeue-Methode: Laufzeit in O-Notation beträgt o(1), da alle Anweisungen der Methode o(1) haben 
+	// Dequeue-Methode: Laufzeit in O-Notation beträgt o(1), 
+	// da alle Anweisungen der Methode o(1) haben 
 	public T dequeue() throws QueueUnderflow {
 		if (queueIsEmpty) {
-			throw new QueueUnderflow("Queue leer");
+			throw new QueueUnderflow("Queue ist leer");
 		}
-		// Queue leer, wenn beide Zeiger nach dequeue auf gleichen Index zeigen 
-		// (Modulo-Funktion um Array als Ringspeicher zu betrachten)
-		queueIsEmpty = (firstFreeIndex == (firstUsedIndex+1) % size);
+		// Queue ist leer, wenn beide Zeiger nach dequeue auf gleichen Index zeigen 
+		// (Modulo-Funktion um Array als Ringspeicher zu verwenden)
+		queueIsEmpty = (firstFreeIndex == (firstUsedIndex+1) % queueSize);
 		T dequeuedObject = (T) array[firstUsedIndex];
 		array[firstUsedIndex] = null;
-		firstUsedIndex = ++firstUsedIndex % size;
+		firstUsedIndex = ++firstUsedIndex % queueSize;
 		queueIsFull = false;
 		return dequeuedObject;
 	}
